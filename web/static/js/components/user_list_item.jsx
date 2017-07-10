f0acfe3c5f50ccfb04f33a167a06cd5638e3a4b6
Sean Marzug-McCarthy <samdec11@gmail.com>
2017-07-10 17:16:20 -0400

    Added SimpleWebRTC

--- web/static/js/components/user_list_item.jsx
+++ web/static/js/components/user_list_item.jsx
@@ -1,18 +1,30 @@
 import React from "react"
+import SimpleWebRTC from "simplewebrtc"
 import * as AppPropTypes from "../prop_types"
 import styles from "./css_modules/user_list_item.css"
 import AnimatedEllipsis from "./animated_ellipsis"
 
 const UserListItem = ({ user }) => {
+  const webrtc = new SimpleWebRTC({
+    localVideoEl: "video",
+    remoteVideosEl: "other",
+    autoRequestMedia: true,
+  })
   let givenName = user.given_name
-  const imgSrc = user.picture.replace("sz=50", "sz=200")
+  // const imgSrc = user.picture.replace("sz=50", "sz=200")/
+
+  webrtc.on("readyToCall", () => {
+    console.dir(webrtc)
+    webrtc.joinRoom("the room where it happens")
+  })
+        // <img className={styles.picture} src={imgSrc} alt={givenName} />
 
   if (user.is_facilitator) givenName += " (Facilitator)"
   return (
     <li className={`item ${styles.wrapper}`}>
       <div className="ui center aligned grid">
-        <img className={styles.picture} src={imgSrc} alt={givenName} />
         <div className="ui row">
+          <video id="video" className={styles.picture} />
           <p className={styles.name}>{ givenName }</p>
           <p className={`${styles.animatedEllipsisWrapper} ui row`}>
             { user.is_typing && <AnimatedEllipsis /> }
